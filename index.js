b9fefcc4813cf4d3563e552584053ec37a658728
ansible-comme <gregdev88@gmail.com>
2019-05-02 10:08:49 +0100

    Video play in index.js
    based on: https://facebook.github.io/react-360/docs/photos-and-videos.html#from-react-1
    
    "You can also combine these techniques to create a video in the runtime, and layer play it from your React application."

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import {
   AppRegistry,
-  View
+  View,
+  //Import NativeModules
+  NativeModules,
 } from 'react-360';
+
+//Create ref
+const {VideoModule} = NativeModules;
+
+//You can use componentDidMount to play immediately
+componentDidMount() {
+  Environment.setBackgroundVideo('myplayer');
+  VideoModule.resume('myplayer');
+}
+
+//Or through click event
+play() {
+  Environment.setBackgroundVideo('myplayer');
+  VideoModule.resume('myplayer');
+}
 
 export default class edition_13th extends React.Component {
   render() {
     return (
       <View>
-
+      <VrButton
+            onClick={this.play.bind(this)}>
+            <Text>
+              Play
+            </Text>
+          </VrButton>
       </View>
     );
   }
