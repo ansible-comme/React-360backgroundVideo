7e9ee1ab9c43642245c6dc67f87b5d7a09889f8a
NataRich <46753484+NataRich@users.noreply.github.com>
2019-05-02 13:24:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  AppRegistry,
+  View
+} from 'react-360';
+
+export default class edition_13th extends React.Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('edition_13th', () => edition_13th);
